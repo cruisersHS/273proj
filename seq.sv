e77537787bdb30611381136f58992ee8726bed4b
cruisers <89080385@qq.com>
2021-04-14 16:00:49 -0700

    Create seq.sv

---
+++ seq.sv
@@ -0,0 +1,15 @@
+// This is our sequence
+
+class seq1 extends uvm_sequence #(our_seq_item);
+`uvm_object_utils(seq1)
+
+function new(string name="seq1");
+	super.new(name);
+endfunction : new
+
+//tasks
+
+
+//
+
+endclass : seq1
